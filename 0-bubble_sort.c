216d7845e8f8879fbd280f12f0f5ee06d0e5c799
Gerald Mulero <gdmasm@gmail.com>
2025-04-22 01:03:03 -0400

    addedbubble sort files after cleaning repository

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers in an array
+ * @a: pointer to first integer
+ * @b: pointer to second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+
+		if (!swapped)
+			break;
+	}
+}
---
+++ sort.h
@@ -0,0 +1,25 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - doubly linked list node
+ * @n: interger stored in the node
+ * @prev: pointer to the previous element
+ * @next: pointer to the next element
+ *
+ * description: doubly linked list node structure
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *aray, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+#endif
